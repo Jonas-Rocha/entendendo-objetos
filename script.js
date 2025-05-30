379d66eb7df49ea47ac83bff1ef7f3b7eafa5ae0
Jonas Rocha <jonasrpnstudy@gmail.com>
2025-05-30 17:05:56 -0300

    Criando um objeto

---
+++ script.js
@@ -0,0 +1,32 @@
+/*
+    OBJETO
+    - Uma coleção de dados e/ou funcionalidades;
+    - Pode ter propriedades e métodos.
+*/
+
+// Cria um objeto vazio.
+
+const obj = {};
+console.log(obj);
+console.log(typeof obj);
+
+// Cria um objeto com propriedades de métodos.
+
+const user = {
+  email: "jonasrpnstudy@gmail.com", // Usamos os dois pontos : para atribuição dentro dos objetos, diferentemente do = que usamos fora para atribuição de variáveis
+  age: 26,
+  name: {
+    firt_name: "Jonas",
+    surname: "Rocha",
+    // Posso ter um objeto dentro de um outro objeto.
+  },
+  address: {
+    street: "Rua X",
+    number: 160,
+    city: "São Paulo",
+    postal_code: "12345-123",
+  },
+  message: () => {
+    console.log("Oi Jonas");
+  },
+};
